b276f35c50658045b84d362c0e4833caee1b4be5
rajumia648 <rajumia648628@gmail.com>
2022-02-06 16:37:59 +0600

    trial dart

---
+++ lib/naeem/trial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Trial extends StatefulWidget {
+  const Trial({Key? key}) : super(key: key);
+
+  @override
+  _TrialState createState() => _TrialState();
+}
+
+class _TrialState extends State<Trial> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
